e503306ccf455e5b5c3b9cd62501293c49eb4ac3
AlanHawking <ureys2hq@gmail.com>
2017-10-13 17:44:25 +0800

    Add food list classes

--- app/src/main/java/com/kcunit/wtev2/FoodListActivity.kt
+++ app/src/main/java/com/kcunit/wtev2/FoodListActivity.kt
@@ -10,12 +10,26 @@ import android.widget.Toast
 
 class FoodListActivity : AppCompatActivity() {
 
+    /**
+     * 菜品数据
+     */
+    private val foodDatas=ArrayList<FoodBriefItem>()
+
+    private lateinit var thisAdapter:BaseAdapterPlus<FoodBriefItem>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_list)
 
         //增加ActionBar回退按钮
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //初始化adapter
+        thisAdapter=object :BaseAdapterPlus<FoodBriefItem>(foodDatas,R.layout.activity_food_list){
+            override fun bindView(viewHolder: ViewHolder, obj: FoodBriefItem?) {
+
+            }
+        }
     }
 
     /**
@@ -53,5 +67,12 @@ class FoodListActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         Toast.makeText(this,"回退按钮已点击",Toast.LENGTH_LONG).show()
         return super.onSupportNavigateUp()
+    }
+
+    /**
+     * 绑定列表数据
+     */
+    private fun bindListDatas(){
+
     }
 }
