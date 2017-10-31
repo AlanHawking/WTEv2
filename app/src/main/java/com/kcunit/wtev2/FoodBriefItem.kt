59843e22c7a8206446a05af8ddcaa5e51ffc1865
AlanHawking <ureys2hq@gmail.com>
2017-10-31 10:22:12 +0800

    daily checkin(serverDataProvider d3)

--- app/src/main/java/com/kcunit/wtev2/FoodBriefItem.kt
+++ app/src/main/java/com/kcunit/wtev2/FoodBriefItem.kt
@@ -1,11 +1,14 @@
 package com.kcunit.wtev2
 
+import com.google.gson.Gson
+
 /**
  * 食品简介项
  * Created by Administrator on 2017/10/11.
  */
 data class FoodBriefItem (var FoodName:CharSequence, var FoodPriceMax:Int, var FoodPriceMin:Int, var FoodRate:Float, override var itemId: Int): ServerDataBase(),IListItemBase {
     override fun readJsonString(data: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val gson= Gson()
+        gson.fromJson<FoodBriefItem>(data,FoodBriefItem::class.java)
     }
 }
--- app/src/main/java/com/kcunit/wtev2/ServerDataProvider.kt
+++ app/src/main/java/com/kcunit/wtev2/ServerDataProvider.kt
@@ -2,18 +2,21 @@ package com.kcunit.wtev2
 
 import java.net.URL
 import java.util.*
-import android.R.attr.data
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.io.ByteArrayOutputStream
 import java.net.HttpURLConnection
+import java.io.PrintWriter
 
 
 /**
  * Created by Administrator on 2017/10/25.
  * 服务器数据提供类
  */
-class ServerDataProvider private constructor(val hostInfo:String){
+class ServerDataProvider private constructor(hostInfo:String,private val requestMethod:RequestMethod){
 
-    val url=URL(hostInfo)
+    private val url=URL(hostInfo)
+    private val gson= Gson()
 
     companion object {
         /**
@@ -24,22 +27,57 @@ class ServerDataProvider private constructor(val hostInfo:String){
         /**
          * 获取数据提供实例
          */
-        fun getProvider(serverInfo: String):ServerDataProvider{
+        fun getProvider(serverInfo: String,requestMethod: RequestMethod):ServerDataProvider{
             var provider=dicProvider[serverInfo]
 
             if(provider==null){
-                provider= getProvider(serverInfo)
+                provider= ServerDataProvider(serverInfo,requestMethod)
+                dicProvider.plus(Pair(serverInfo,provider))
             }
 
             return provider
         }
+
+        enum class RequestMethod{GET,POST,PUT}
     }
 
-    fun getData(params:String):String {
+    /**
+     * 从远程服务器中获取指定类型的数据
+     * @param params 传到服务器的参数
+     * @return 返回数据
+     */
+    fun <T> getData(params:String):T {
+        val jsonData=getJson(params)
+
+        return gson.fromJson(jsonData,object:TypeToken<T>(){}.type)
+    }
+
+    /**
+     * 获取指定类型的数据List
+     * @param params 传到服务器的参数
+     * @return 指定类型的数据List
+     */
+    fun <T> getList(params:String):List<T>{
+        val jsonData=getJson(params)
+
+        return gson.fromJson(jsonData,object:TypeToken<List<T>>(){}.type)
+    }
+
+    /**
+     * 传入参数并获得返回Json字符串
+     */
+    private fun getJson(params:String):String{
+        //开启链接并设置连接方式
         val conn = url.openConnection() as HttpURLConnection
+        conn.requestMethod=requestMethod.name.toUpperCase()
 
-        conn.outputStream.write(params.toByteArray())
+        //传入参数
+        val pw = PrintWriter(conn.outputStream)
+        pw.print(params)
+        pw.flush()
+        pw.close()
 
+        //接受数据
         val inStream = conn.inputStream
         val data = ByteArray(1024)
         val outStream = ByteArrayOutputStream()
@@ -51,6 +89,7 @@ class ServerDataProvider private constructor(val hostInfo:String){
 
         inStream.close()
 
+        //json解析转化
         return outStream.toByteArray().toString()
     }
 }
